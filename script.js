f5b42d4cfb53433ee49f4a163b34dab8761c1f13
AfiaGhanchi <afiaghanchi77@gmail.com>
2024-07-26 01:58:33 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+    const length = document.getElementById('passwordLength').value;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById('generatedPassword').innerText = password;
+}
